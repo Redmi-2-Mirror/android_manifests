939c51abdec82af99b5da2de500a8a51cf51e4c5
Ricky Cheung <rcheung844@gmail.com>
2022-02-27 18:13:20 +0800

    Update picks
    Change-Id: If7358db41e847837a8c2904ba6c2e0047055d8fa

--- patch.sh
+++ patch.sh
@@ -1,3 +1,4 @@
+# FIXME: Remove and backport BPF instead
 # Ultra Legacy
 repopick -f -P art 318097
 repopick -f 287706 -P external/perfetto
@@ -6,6 +7,9 @@ repopick 321934
 repopick -f -P system/bpf 320591
 repopick -f -P system/netd 320592
 
+# packages/apps/Etar
+repopick 319630-319634
+
 # Camera
 repopick -t twelve-restore-camera-hal1
 repopick -t twelve-camera-extension
@@ -13,10 +17,16 @@ repopick 320528-320530                              # twelve-qcom-cam
 repopick -P hardware/interfaces 320531-320532       # twelve-qcom-cam
 repopick -t twelve-legacy-camera
 
+# Encryption
+repopick -P system/vold 322888-322897               # twelve-qcom-fde
+repopick 322898-322904                              # twelve-qcom-fde
+
 # Extras
 repopick 320514
 
 # Sepolicy
 repopick 320333
-repopick 324005
-repopick 324006
+repopick 324005-324006
+
+# WiFi
+repopick 325387
