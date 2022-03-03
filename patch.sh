5f9f6a19f2b074ce803d72683cd3ef703e153fd3
Ricky Cheung <rcheung844@gmail.com>
2022-03-03 10:35:52 +0800

    Modify for 18.1

--- patch.sh
+++ patch.sh
@@ -1,35 +1,5 @@
-# FIXME: Remove and backport BPF instead
-# Ultra Legacy
-repopick -f -P art 318097
-repopick -f 287706 -P external/perfetto
-repopick 318458
-repopick 321934
-repopick -f -P system/bpf 320591
-repopick -f -P system/netd 320592
-
-# packages/apps/Etar
-repopick 319630-319634
-
-# Camera
-repopick -t twelve-restore-camera-hal1
-repopick -t twelve-camera-extension
-repopick 320528-320530                              # twelve-qcom-cam
-repopick -P hardware/interfaces 320531-320532       # twelve-qcom-cam
-repopick -t twelve-legacy-camera
-
-# Encryption
-repopick -P system/vold 322888-322899               # twelve-qcom-fde
-repopick 322898-322904                              # twelve-qcom-fde
-
-# Extras
-repopick 320514
-
 # Sensots
-repopick -f -t max-mag-field
-
-# Sepolicy
-repopick 320333
-repopick 324005-324006
+repopick -t max-mag-field
 
 # WiFi
-repopick 325387
+repopick 325295
